c01aa65cb615737104257360ec0b1a776f32dcd8
Yaman jaddouh <jad.yaman.jad@gmail.com>
2024-02-26 22:42:18 +0300

    js & css  editing and python
    adding selected icon to image selected , add uploading images to flask , handle the uploading image and saveThem

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,send_from_directory,send_file
+from flask import Flask,render_template,send_from_directory,send_file,request
 
 app = Flask(__name__)
 
@@ -13,5 +13,12 @@ def sendImages(filename):
 
     # return send_file(f"image\{filename}")
     return send_from_directory('images',filename)
-
+import os 
+@app.route('/ImageUploaded' , methods=['POST'])
+def handle():
+    print()
+    files = request.files.getlist('files')
+    for file in files:
+        file.save(os.path.join(os.path.join(app.instance_path,'uploads'), file.filename))
+    return "as"
 app.run(host='0.0.0.0',port=5000)
--- static/js/main.js
+++ static/js/main.js
@@ -1,82 +1,137 @@
-let numofimg=0; 
+let numofimg = 0;
+let imgSrcArr = [];
+let imageSelectIcons;
+let isImgSelected = [];
+let ImagesUploadedToBackend =[]
+let imageFormData = new FormData()
 //upload image by selceting 
 const loadFile = (event) => {
-
-    for (let i = numofimg; i < ((event.target.files.length)+numofimg); i++) {
+    for (let i = numofimg; i < ((event.target.files.length) + numofimg); i++) {
         //creat a container for each image
         let imageCont = document.createElement('div');
         imageCont.className = "imageCont";
         imageCont.id = "imageCont-" + i;
         document.querySelector(".cont").appendChild(imageCont);
-
+        addSelectImg(i);
         // get images link and creat images
         let image = document.createElement('img');
-        image.src = URL.createObjectURL(event.target.files[i-numofimg]);
+        image.src = URL.createObjectURL(event.target.files[i - numofimg]);
+        imgSrcArr.push(image.src);
         image.id = `imageNum-` + i;
         document.querySelector("#imageCont-" + i).appendChild(image);
-
-        //get a natural size for image and creat a span for each image to show the size of image
-        image.onload = ()=>{
-            getSize(image, i);
-        }
+        imageFormData.append('files', event.target.files[i - numofimg])
+        imageFormData.append('name', event.target.files[i - numofimg].name)        
     }
+
+    fetch(`${ window.location.href}/ImageUploaded`, {
+        method: 'POST',
+        body: imageFormData,
+    }).then(function (response) {
+        console.log(response)
+        imageFormData =new FormData();
+    })
     let upload = document.getElementById("upload").classList.add("disableUpload")
-    let moreUpload = document.getElementById("moreUpload").classList.remove("disableUpload") 
+    let moreUpload = document.getElementById("moreUpload").classList.remove("disableUpload")
     numofimg += event.target.files.length;
+    imageSelectIcons = document.querySelectorAll(".selectIcon");
+    imageSelectIcons.forEach(getImgSelected);
 };
 
 //upload image by drag and drop
 const dragging = (e) => {
     e.preventDefault();
-  };
+};
 
 const onDrop = (event) => {
     event.preventDefault();
     const { files } = event.dataTransfer;
-    for (let i = numofimg; i < (files.length)+numofimg; i++) {
+    console.log(files)
+    for (let i = numofimg; i < (files.length) + numofimg; i++) {
         let imageCont = document.createElement('div');
         imageCont.className = "imageCont";
         imageCont.id = "imageCont-" + i;
         document.querySelector(".cont").appendChild(imageCont);
 
+        addSelectImg(i);
+
         // get images link and creat images
         let image = document.createElement('img');
-        image.src = URL.createObjectURL(files[i-numofimg]);
+        image.src = URL.createObjectURL(files[i - numofimg]);
+        imgSrcArr.push(image.src);
         image.id = `imageNum-` + i;
         document.querySelector("#imageCont-" + i).appendChild(image);
-
+        imageFormData.append('files', files[i - numofimg])
+        imageFormData.append('name', files[i - numofimg].name)   
         //get a natural size for image and creat a span for each image to show the size of image
-        image.onload = function(){
+        image.onload = function () {
             getSize(image, i);
         }
+
     }
+    fetch(`${ window.location.href}/ImageUploaded`, {
+        method: 'POST',
+        body: imageFormData,
+    }).then(function (response) {
+        console.log(response)
+        imageFormData =new FormData();
+    })
     let upload = document.getElementById("upload").classList.add("disableUpload")
-    let moreUpload = document.getElementById("moreUpload").classList.remove("disableUpload") 
-    numofimg+=files.length;
-  };
+    let moreUpload = document.getElementById("moreUpload").classList.remove("disableUpload")
+    numofimg += files.length;
+    imageSelectIcons = document.querySelectorAll(".selectIcon");
+};
 
 
 
 // get image size (width*height) and add span element under the image in the page
-function getSize(image, i){
+function getSize(image, i) {
     let imgWidth = image.naturalWidth;
     let imgHight = image.naturalHeight;
     let size = document.createElement('span');
-    let txt = document.createTextNode(imgWidth + "x" + imgHight);
+    let txt = document.createTextNode(imgWidth + "x" + imgHight + " px");
     size.id = "sizeOfImgNum-" + i;
     size.appendChild(txt)
     document.querySelector("#imageCont-" + i).appendChild(size);
 }
 
 
+function addSelectImg(i) {
+    let selectIcon = document.createElement('span');
+    selectIcon.classList.add("selectIcon")
+    selectIcon.id = "selectIcon" + i;
+    document.querySelector("#imageCont-" + i).appendChild(selectIcon);
+}
+
+
+function getImgSelected(item) {
+    item.addEventListener('click', function () {
+        if (!(item.classList.contains("bgFill"))) {
+            item.classList.add("bgFill")
+            isImgSelected.push(document.getElementById("imageNum-" + item.id.slice(10, item.id.length)).src)
+            console.log(isImgSelected)
+
+        }
+        else {
+            item.classList.remove("bgFill")
+            isImgSelected = isImgSelected.filter(e => e !== (document.getElementById("imageNum-" + item.id.slice(10, item.id.length)).src))
+            // console.log(isImgSelected)
+        }
+
+    })
+
+}
+
 let ResizeHeader = document.querySelector('.ResizeHeader')
-ResizeHeader.addEventListener("click", function() {
-    let resizeCont= document.querySelector('.resizeCont')
+ResizeHeader.addEventListener("click", function () {
+    let resizeCont = document.querySelector('.resizeCont')
     resizeCont.classList.remove("dsiable")
 })
 
 let ResizeCancle = document.querySelector('.ResizeCancle')
-ResizeCancle.addEventListener('click', function(){
-    let resizeCont= document.querySelector('.resizeCont')
+ResizeCancle.addEventListener('click', function () {
+    let resizeCont = document.querySelector('.resizeCont')
     resizeCont.classList.add("dsiable")
 })
+
+
+
